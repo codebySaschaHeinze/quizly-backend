3c4204a6bfbb55fd4cb9022e67f3aff7126abfa9
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-04-07 19:46:02 +0200

    feat(core): add quiz routing

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.api.urls')),
+    path('api/', include('quizzes.api.urls')),
 ]
